40e151453a8908a5f31d3a6275e7ae00794967d9
Aytechy <isrealdurotimi001@gmail.com>
2025-08-16 00:56:53 +0100

    debugging

--- api/chat.ts
+++ api/chat.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
+// ES Module export instead of CommonJS export
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     console.log('=== API Route Debug Info ===');
