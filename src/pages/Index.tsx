623af5f57c8a3e57f27fd5e83ab4432bc4fd0602
Aytechy <isrealdurotimi001@gmail.com>
2025-08-16 22:04:19 +0100

    new Testing

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -96,7 +96,7 @@ const Index: React.FC = () => {
   }, [error, toast]);
 
   return (
-    <div className="h-full flex flex-col">
+    <div className="fixed inset-0 flex flex-col">
       <ClipboardUpload onImagePaste={(file) => {
         if (document.querySelector('[data-prompt-card]')) {
           const event = new CustomEvent('pastedImage', { detail: file });
