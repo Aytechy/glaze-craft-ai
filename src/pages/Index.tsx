29959cdec8fab439b9abed91c30672c004ddef15
Aytechy <isrealdurotimi001@gmail.com>
2025-08-16 14:43:10 +0100

    Just testing

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -159,6 +159,9 @@ const Index: React.FC = () => {
             {/* Fixed PromptCard at bottom */}
             <div 
               className="fixed bottom-0 inset-x-0 mx-auto z-30 bg-background/95 backdrop-blur border-t"
+              style={{
+                paddingBottom: 'calc(env(safe-area-inset-bottom) + 16px)',
+              }}
             >
               <PromptCard
                 onSendMessage={handleSendMessage}
