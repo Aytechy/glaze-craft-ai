f296ce9812f0476c0b1127ccec794616d49752e3
Aytechy <isrealdurotimi001@gmail.com>
2025-08-17 03:09:49 +0100

    “update”

--- src/components/ProfilePopup.tsx
+++ src/components/ProfilePopup.tsx
@@ -35,7 +35,7 @@ export const ProfilePopup: React.FC<ProfilePopupProps> = ({ isOpen, onClose, use
     <>
       {/* Backdrop */}
       <div 
-        className="fixed inset-0 z-44"
+        className="fixed inset-0 z-60"
         onClick={onClose}
       />
       
